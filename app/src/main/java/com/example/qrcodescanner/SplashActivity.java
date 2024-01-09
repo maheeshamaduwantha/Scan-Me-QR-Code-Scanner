31b0162fc68e3f0d39c239077f61b99e951b6fef
Maheesha Maduwantha Premathilaka <madhuwanthamaheesha@gmail.com>
2024-01-09 22:39:40 +0530

    Final Commit

---
+++ app/src/main/java/com/example/qrcodescanner/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.qrcodescanner;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Button scanBtn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        scanBtn = findViewById(R.id.scanNow);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
